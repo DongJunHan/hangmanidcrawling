7455bb544de686dff807eb5ad7544dcaac5b914b
DongJunHan <gamedokdok@naver.com>
2023-02-04 10:54:07 +0900

    미완성 당첨내역정보

--- lotto_store_crawling.py
+++ lotto_store_crawling.py
@@ -9,6 +9,8 @@ from typing import ClassVar
 from typing import List
 import jdbc
 import uuid
+from datetime import datetime
+import re
 
 
 @dataclass(unsafe_hash=True)
@@ -47,6 +49,8 @@ class StoreInfo:
     storeMobileNum : str                                       #상점 핸드폰번호
     storeOpenTime  : str                                       #영업 시작 시간
     storeCloseTime : str                                       #영업 폐점 시간
+    storesido      : str                                       #상점 시/도
+    storesigugun   : str                                       #상점 시구군
     storeisActivity: bool = False                              #폐점 여부
     lottoHandle    : ClassVar[LottoHandleList]                 #취급 복권 리스트
     # winHistory     : WinHistory                                #당첨 내역
@@ -71,32 +75,32 @@ class StoreInfo:
 
 
 address_map = {
-    "서울" : ["강남구","강동구","강북구","강서구","관악구","광진구","구로구","금천구","노원구","도봉구","동대문구","동작구","마포구","서대문구","서초구",
-            "성동구","성북구","송파구","양천구","영등포구","용산구","은평구","종로구","중구","중랑구"],
-    "경기" : ["가평군","고양시 덕양구","고양시 일산동구","고양시 일산서구","과천시","광명시","광주시","구리시","군포시","김포시","남양주시","동두천시",
-            "부천시","성남시 분당구","성남시 수정구","성남시 중원구","수원시 권선구","수원시 영통구","수원시 장안구","수원시 팔달구","시흥시","안산시 단원구",
-            "안산시 상록구","안성시","안양시 동안구","안양시 만안구","양주시","양평군","여주시","연천군","오산시","용인시 기흥구","용인시 수지구","용인시 처인구",
-            "의왕시","의정부시","이천시","파주시","평택시","포천시","하남시","화성시"],
-    "부산" : ["강서구","금정구","기장군","남구","동구","동래구","부산진구","북구","사상구","사하구","서구","수영구","연제구","영도구","중구","해운대구"],
-    "대구" : ["남구","달서구","달성군","동구","북구","서구","수성구","중구"],
-    "인천" : ["강화군","계양구","남동구","동구","미추홀구","부평구","서구","연수구","옹진군","중구"],
-    "대전" : ["대덕구","동구","서구","유성구","중구"],
-    "울산" : ["남구","동구","북구","울주군","중구"],
-    "강원" : ["강릉시","고성군","동해시","삼척시","속초시","양구군","양양군","영월군","원주시","인제군","정선군","철원군","춘천시","태백시","평창군",
-            "홍천군","화천군","횡성군"],
-    "충북" : ["괴산군","단양군","보은군","영동군","옥천군","음성군","제천시","증평군","진천군","청주시 상당구","청주시 서원구","청주시 청원구",
-            "청주시 흥덕구","충주시"],
-    "충남" : ["계룡시","공주시","금산군","논산시","당진시","보령시","부여군","서산시","서천군","아산시","예산군","천안시 동남구","천안시 서북구",
-            "청양군","태안군","홍성군"],
-    "광주" : ["광산구","남구","동구","북구","서구"],
-    "전북" : ["고창군","군산시","김제시","남원시","무주군","부안군","순창군","완주군","익산시","임실군","장수군","전주시 덕진구","전주시 완산구",
-            "정읍시","진안군"],
-    "전남" : ["강진군","고흥군","곡성군","광양시","구례군","나주시","담양군","목포시","무안군","보성군","순천시","신안군","여수시","영광군","영암군",
-            "완도군","장성군","장흥군","진도군","함평군","해남군","화순군"],
-    "경북" : ["경산시","경주시","고령군","구미시","군위군","김천시","문경시","봉화군","상주시","성주군","안동시","영덕군","영양군","영주시","영천시",
-            "예천군","울릉군","울진군","의성군","청도군","청송군","칠곡군","포항시 남구","포항시 북구"],
-    "경남" : ["거제시","거창군","고성군","김해시","남해군","밀양시","사천시","산청군","양산시","의령군","진주시","창녕군","창원시 마산합포구","창원시 마산회원구",
-            "창원시 성산구","창원시 의창구","창원시 진해구","통영시","하동군","함안군","함양군","합천군"],
+    # "서울" : ["강남구","강동구","강북구","강서구","관악구","광진구","구로구","금천구","노원구","도봉구","동대문구","동작구","마포구","서대문구","서초구",
+    #         "성동구","성북구","송파구","양천구","영등포구","용산구","은평구","종로구","중구","중랑구"],
+    # "경기" : ["가평군","고양시 덕양구","고양시 일산동구","고양시 일산서구","과천시","광명시","광주시","구리시","군포시","김포시","남양주시","동두천시",
+    #         "부천시","성남시 분당구","성남시 수정구","성남시 중원구","수원시 권선구","수원시 영통구","수원시 장안구","수원시 팔달구","시흥시","안산시 단원구",
+    #         "안산시 상록구","안성시","안양시 동안구","안양시 만안구","양주시","양평군","여주시","연천군","오산시","용인시 기흥구","용인시 수지구","용인시 처인구",
+    #         "의왕시","의정부시","이천시","파주시","평택시","포천시","하남시","화성시"],
+    # "부산" : ["강서구","금정구","기장군","남구","동구","동래구","부산진구","북구","사상구","사하구","서구","수영구","연제구","영도구","중구","해운대구"],
+    # "대구" : ["남구","달서구","달성군","동구","북구","서구","수성구","중구"],
+    # "인천" : ["강화군","계양구","남동구","동구","미추홀구","부평구","서구","연수구","옹진군","중구"],
+    # "대전" : ["대덕구","동구","서구","유성구","중구"],
+    # "울산" : ["남구","동구","북구","울주군","중구"],
+    # "강원" : ["강릉시","고성군","동해시","삼척시","속초시","양구군","양양군","영월군","원주시","인제군","정선군","철원군","춘천시","태백시","평창군",
+    #         "홍천군","화천군","횡성군"],
+    # "충북" : ["괴산군","단양군","보은군","영동군","옥천군","음성군","제천시","증평군","진천군","청주시 상당구","청주시 서원구","청주시 청원구",
+    #         "청주시 흥덕구","충주시"],
+    # "충남" : ["계룡시","공주시","금산군","논산시","당진시","보령시","부여군","서산시","서천군","아산시","예산군","천안시 동남구","천안시 서북구",
+    #         "청양군","태안군","홍성군"],
+    # "광주" : ["광산구","남구","동구","북구","서구"],
+    # "전북" : ["고창군","군산시","김제시","남원시","무주군","부안군","순창군","완주군","익산시","임실군","장수군","전주시 덕진구","전주시 완산구",
+    #         "정읍시","진안군"],
+    # "전남" : ["강진군","고흥군","곡성군","광양시","구례군","나주시","담양군","목포시","무안군","보성군","순천시","신안군","여수시","영광군","영암군",
+    #         "완도군","장성군","장흥군","진도군","함평군","해남군","화순군"],
+    # "경북" : ["경산시","경주시","고령군","구미시","군위군","김천시","문경시","봉화군","상주시","성주군","안동시","영덕군","영양군","영주시","영천시",
+    #         "예천군","울릉군","울진군","의성군","청도군","청송군","칠곡군","포항시 남구","포항시 북구"],
+    # "경남" : ["거제시","거창군","고성군","김해시","남해군","밀양시","사천시","산청군","양산시","의령군","진주시","창녕군","창원시 마산합포구","창원시 마산회원구",
+    #         "창원시 성산구","창원시 의창구","창원시 진해구","통영시","하동군","함안군","함양군","합천군"],
     "제주" : ["서귀포시","제주시"],
     "세종" : []
 }
@@ -104,8 +108,62 @@ import requests
 import json
 import html
 from html.parser import HTMLParser
-class ParseStore:
+"""
+    TODO. 
+    1. 운영시 매주 한 번 프로그램을 돌려서 상점이 새로 추가됐는지, 기존의 상점이 없어졌는지 확인이 필요.
+    2. 당첨 결과 매주 업데이트 로직.
+    3. 당첨 결과 262회부터 파싱.
 
+"""
+class WinHTMLParser(HTMLParser):
+    is_group_content = False
+    first_result = []
+    second_result = []
+    first_win = False
+    second_win = False
+    def __init__(self):
+        HTMLParser.__init__(self)
+        self.is_group_content = False
+    def handle_starttag(self, tag, attrs):
+        if tag == "div":
+            for i in attrs:
+                if i[0] == "class" and i[1] == "group_content":
+                    self.is_group_content = True
+    
+    def handle_endtag(self, tag):
+        if tag == "table":
+            self.is_group_content = False
+    
+    def handle_data(self, data):
+        if self.is_group_content == True:
+            if data == "1등 배출점":
+                self.first_win = True
+                self.second_win = False
+                return
+            if data == "2등 배출점":
+                self.first_win = False
+                self.second_win = True
+                return
+            data = data.replace("\n","").replace("\t","").replace(" ","")
+            if len(data) != 0:
+                if self.first_win:
+                    self.first_result.append(data)
+                elif self.second_win:
+                    self.second_result.append(data)
+class Utils:
+    def get_current_time():
+        """
+            Args:
+                None
+            Returns:
+                timeformat: str
+                dateformat: str
+        """
+        current = datetime.now()
+        time = current.strftime("%H:%M:S")
+        date = current.strftime("%y%m%d")
+        return date,time
+class ParseStore:
     def _remove_special_symbol(self, text):
         """
             Args:
@@ -117,11 +175,14 @@ class ParseStore:
         while "&#" in text:
             text = escape.unescape(text)
         return text
-    def compareStores(self, originData, compareData):
+
+    def compareStores(self, originData, compareData, sido, sigugun):
         """
             Args:
-                originData : dict
+                originData  : dict
                 compareData : dict
+                sido        : str
+                sigugun     : str
             Return:
                 list[StoreInfo]
         """
@@ -148,13 +209,13 @@ class ParseStore:
                             # break
 
             if equalFlag == False:
-                store = self._setting_storeinfo_object(compareData[i])
+                store = self._setting_storeinfo_object(compareData[i], sido, sigugun)
                 result.append(store)
                 # print(store.__str__())
                 #try save lotto645 data
                 pass
         for data in originData:
-            store = self._setting_storeinfo_object(data)
+            store = self._setting_storeinfo_object(data, sido, sigugun)
             result.append(store)
             # print(store.__str__())
         return result
@@ -205,10 +266,12 @@ class ParseStore:
             "SPEETTO2000_YN": "Y"
         }   
     """
-    def _setting_storeinfo_object(self, storeData:dict):
+    def _setting_storeinfo_object(self, storeData:dict, sido, sigugun):
         """
             Args:
                 storeData: dict #json으로 응답받은 상점정보
+                sido     : str
+                sigugun  : str
             Return:
                 StoreInfo
         """
@@ -218,6 +281,8 @@ class ParseStore:
         lottoList = list()
         storeInfo.storeOpenTime = None
         storeInfo.storeCloseTime = None
+        storeInfo.storesido = sido
+        storeInfo.storesigugun = sigugun
         #set uuid
         storeInfo.storeUuid = str(uuid.uuid1())
         if "LONGITUDE" in storeData.keys():
@@ -342,11 +407,11 @@ class ParseStore:
 
 
 
-    def parseStoreInfo(self, url, gugun, sido, queryString:dict=None):
+    def parseStoreInfo(self, url, sido, sigugun, queryString:dict=None):
         """
             Args:
                 url : str
-                gugun : str 주소 구/군
+                sigugun : str 주소 구/군
                 sido : str 주소 시/도
                 queryString : dict
             Return:
@@ -364,7 +429,7 @@ class ParseStore:
             "searchType":"1",
             "nowPage": "1",
             "rtlrSttus": "001",
-            "sltGUGUN": gugun,
+            "sltGUGUN": sigugun,
             "sltSIDO": sido
         }
         param = {}
@@ -378,7 +443,7 @@ class ParseStore:
             response = session.request("POST",url,headers=headers, data=postData, params=param)
             jsonData = response.json()
             # with open("log.log", "a") as f:
-                # f.write(f"[{sido}][{gugun}] nowPage: {postData['nowPage']}, query string: {param['method']}\n")
+                # f.write(f"[{sido}][{sigugun}] nowPage: {postData['nowPage']}, query string: {param['method']}\n")
             try:
                 if validateInfo is not None:
                     if "SHOP_NM" in jsonData["arr"][0].keys():
@@ -403,9 +468,16 @@ class ParseStore:
                 for i in jsonData["arr"]:
                     result.append(i)
             except Exception as e:
-                with open(f"./test/{gugun}_{queryString['method']}_{i}.json","w") as f:
-                    print(jsonData)
+                u = Utils()
+                date, time = u.get_current_time()
+                with open(f"./log/error_log.log","a") as f:
+                    f.writ(f"[{time}] {sigugun},{queryString['method']},{postData['nowPage']}")
+
+                with open(f"./errorDump/{sigugun}_{queryString['method']}_{date}.json","w") as f:
                     f.write(json.dumps(jsonData))
+                """
+                    TODO 개발자가 알 수 있도록 알람을 띄워야할거같음
+                """
                 raise e
         return result
         
@@ -413,9 +485,197 @@ class ParseStore:
         url = "https://dhlottery.co.kr/store.do"
         for key in address_map.keys():
             sido = key
-            for gugun in address_map[key]:
-                speetto = self.parseStoreInfo(url, gugun, sido, {"method":"sellerInfoPrintResult"})
-                lotto645 = self.parseStoreInfo(url, gugun, sido, {"method":"sellerInfo645Result"})
-                storeDataes = self.compareStores(speetto, lotto645)
+            for sigugun in address_map[key]:
+                speetto = self.parseStoreInfo(url, sido, sigugun, {"method":"sellerInfoPrintResult"})
+                lotto645 = self.parseStoreInfo(url, sido, sigugun, {"method":"sellerInfo645Result"})
+                storeDataes = self.compareStores(speetto, lotto645, sido, sigugun)
                 self.save_store_data(storeDataes)
+
+class WinParseException(Exception):
+    #생성할때 value 값을 입력 받는다.
+    def __init__(self, value):
+        self.value = value
+    
+    #생성할때 받은 value 값을 확인 한다.
+    def __str__(self):
+        return self.value
+
+class WinInfo:
+    """
+        1등,2등 당첨지 파싱
+    """
+    lottoTypes = {
+            "lotto645":"L645",
+            "annual":"L720",
+            "speetto500":"SP500",
+            "speetto1000":"SP1000",
+            "speetto2000":"SP2000"
+        }
+    first_win_info = []
+    second_win_info = []
+
+
+    def _find_max_round(self, session, url, headers, postData, param):
+        """
+            Args:
+                session: requests.session
+                url: str
+                headers: dict
+                postData: dict
+                param: dirc
+            Return:
+                str
+        """
+        maxRound = None
+        try:
+            response = session.request("POST",url,headers=headers, data=postData, params=param)
+            restxt = response.text
+            if "drwNo" not in restxt:
+                raise WinParseException("회차 파싱 오류: can't find 'drwNo'")
+            
+            p = re.compile("(\<option value=\"[\d]+\"\>)(.*?)(\<\/option\>)")
+            m = p.findall(restxt)
+            maxRound = m[0][1]
+
+        except WinParseException as e:
+            u = Utils()
+            date, time = u.get_current_time()
+            with open(f"./log/error_log.log","a") as f:
+                f.write(f"[{time}] schVal: {postData['schVal']}\n")
+                f.write(f"[{time}] nowPage: {postData['nowPage']}\n")
+            with open(f"./errorDump/{param['pageGubun']}_{date}.html","w") as f:
+                f.write(f"{restxt}")
+        return maxRound
+
+    def _get_first_win_info(self, first_list):
+        if "조회결과가없습니다" in first_list[9]:
+            return None
+        store = {}
+        keys = [first_list[4], first_list[5],first_list[6], first_list[7],first_list[8]]
+        i = 8
+        while True:
+            if len(first_list) <= i:
+                break
+            for k in keys:
+                i += 1
+                store[k] = first_list[i]
+            self.first_win_info.append(store)
+    def _get_second_win_info(self, second_list):
+        if "조회결과가없습니다" in second_list[9]:
+            return None
+        store = {}
+        keys = [second_list[4], second_list[5],second_list[6], second_list[7],second_list[8]]
+        i = 8
+        while True:
+            if len(second_list) <= i:
+                break
+            for k in keys:
+                i += 1
+                store[k] = second_list[i]
+            self.second_win_info.append(store)
+    #method=topStore&pageGubun=L645
+    def _parseAllWinInfoByArea(self, url, sido, sigugun, queryString):
+        """
+            Args:
+                url : str
+                sido : str
+                sigugun: str
+                queryString: dict
+                maxRound: dict
+            Return
+                firstList: list
+                secondList: List
+        """
+        firstList = []
+        secondList = []
+
+        maxRound = None
+        headers = {
+            "Accept" : "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+            "Accept-Encoding" : "gzip, deflate, br",
+            "User-Agent" : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"
+        }
+        param = {}
+        if queryString is not None:
+            for key, value in queryString.items():
+                param[key] = value
         
+        postData = {
+            "method": "topStore",
+            "nowPage": "1",
+            "rangNo": "",
+            "gameNo": "5133",
+            "drwNo":"",
+            "schKey": "area",
+            "schVal": str(sido.encode('euc-kr'),'euc-kr')+" "+str(sigugun.encode('euc-kr'),'euc-kr')
+        }
+        
+        session = requests.Session()
+        session.verify = "FidderRootCertificate.crt"
+        if maxRound is None:
+            maxRound = self._find_max_round(session, url, headers, postData, param)
+        try:
+            for no in range(1, int(maxRound)+1):
+                postData["drwNo"] = str(no)
+                duplicateFirst = None
+                duplicateSecond = None
+                breakCount = 0
+                for i in range(30):
+                    response = session.request("POST",url,headers=headers, data=postData, params=param)
+                    restxt = response.text
+                    #<td class="nodata" colspan="5">조회 결과가 없습니다.</td>
+                    p = re.compile("(\<td class=\"nodata\" colspan=\"[\d]+\"\>)(.*?)(\<\/td\>)")
+                    m = p.findall(restxt)
+                    if m.__len__() == 2:
+                        break
+                    with open(f"./test/{sido}_{sigugun}_{postData['nowPage']}_{postData['gameNo']}.html","w") as f:
+                        f.write(restxt)
+                    htmlParser = WinHTMLParser()
+                    htmlParser.feed(restxt)
+                    
+                    #중복페이지 조회 로직: 조회결과가없을때는 항상 카운터를 올린다.
+                    print(htmlParser.first_result[9])
+                    if "조회결과가없습니다" in htmlParser.first_result[9]:
+                        print("first NO")
+                        breakCount += 1
+                    else:
+                        if duplicateFirst:
+                            if duplicateFirst == htmlParser.first_result[10]:
+                                breakCount += 1
+                        else:
+                            duplicateFirst = htmlParser.first_result[10]
+                    print(htmlParser.second_result[9])
+                    if "조회결과가없습니다" in htmlParser.second_result[9]:
+                        print("second NO")
+                        breakCount += 1
+                    else:
+                        if duplicateSecond:
+                            if duplicateSecond == htmlParser.second_result[10]:
+                                breakCount += 1
+                        else:
+                            duplicateSecond = htmlParser.second_result[10]
+                    
+                    if breakCount == 2:
+                        break
+                    breakCount = 0
+
+                    self._get_first_win_info(htmlParser.first_result)
+                    self._get_second_win_info(htmlParser.second_result)
+
+                    postData["nowPage"] = str(int(postData["nowPage"]) + 1)
+
+        except Exception as e:
+            raise e
+    def getWinInfo(self):
+        queryString = {}
+        queryString["method"] = "topStore"
+        for key, value in self.lottoTypes.items():
+            queryString["pageGubun"] = value
+            for sido in address_map.keys():
+                for sigugun in address_map[sido]:
+                    print(f"lottoType: {value}, sido: {sido}, sigugun: {sigugun}")
+                    self._parseAllWinInfoByArea("https://dhlottery.co.kr/store.do" ,sido, sigugun, queryString)
+
+        
+        print(self.first_win_info)
+        print(self.second_win_info)
