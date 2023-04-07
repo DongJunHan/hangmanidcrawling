b89e487bf2b60eea3f45233696adbe77aae44cf2
DongJunHan <gamedokdok@naver.com>
2023-04-07 14:45:42 +0900

    add unittest

--- test.py
+++ test.py
@@ -15,78 +15,95 @@ class TestStoreInfo(unittest.TestCase):
     def test_store_info(self):
         storeInfo = lotto_config.LottoConfig().parseStoreInfo()
         jdbc_config.JDBCConfig().save_store_data(storeInfo)
+    
+    def test_save_lotto_type(self):
+        jdbc_config.JDBCConfig().save_store_lotto_types()
+
+class TestCompareInfo(unittest.TestCase):
+    def test_store_compare(self):
+        with open('annual_speetto.json') as f:
+            originData = json.load(f)
+        with open('lotto645.json') as f:
+            compareData = json.load(f)
+        ret = data_compare.StoreInfoCompare().compareStores(originData["arr"], compareData["arr"], '서울', '강남구')
+        print(ret)
+
+
+
 class TestWinInfo(unittest.TestCase):
-    dataCompare = data_compare.WinHistoryInfoCompare()
-    save = jdbc_config.JDBCConfig()
+    # dataCompare = data_compare.WinHistoryInfoCompare()
+    # save = jdbc_config.JDBCConfig()
 
     def test_win_info(self):
         first_win_info , second_win_info = lotto_config.LottoConfig().parseWinHistory()
-        with open("./win_history_노원.json", "w") as f:
+        # print(first_win_info)
+        # print(second_win_info)
+        with open("./win_history.json", "w") as f:
             f.write(first_win_info.__str__())
 
-        with open("./win_history_노원_2.json", "w") as f:
+        with open("./win_history_2.json", "w") as f:
             f.write(second_win_info.__str__())
     
     def test_query(self):
-        with open("./win_history_노원.json") as f:
+        with open("./win_history_부평구.json") as f:
             a = json.load(f)
-            firstRankWinHistory = {}
-            firstRankWinHistory["lotto645"] = a
-        with open("./win_history_노원_2.json") as f:
+            firstRankWinHistory = a
+            # firstRankWinHistory = {}
+            # firstRankWinHistory["lotto645"] = a
+        with open("./win_history_부평구_2.json") as f:
             a = json.load(f)
-            secondRankWinHistory = {}
-            secondRankWinHistory["lotto645"] = a
-
+            secondRankWinHistory = a
+            # secondRankWinHistory = {}
+            # secondRankWinHistory["lotto645"] = a
         lotto_config.LottoConfig().compareAndSaveWinHistory(firstRankWinHistory, secondRankWinHistory)
         
         # self.save.save_win_history_data(result)
     
 
     def test_regex(self):
-        file_name = "nodata_1win.html"
+        file_name = "adsasd.html"
         with open(f"./{file_name}","r") as f:
-            html = f.read()
+            response = f.read()
         #parse table tag
-        p = re.compile("(?<=\<table class\=\"tbl_data tbl_data_col\">)(.*?)(?=<\/tbody>)",re.DOTALL)
-        m = p.findall(html)
-        arr = []
-        for i in m:
-            i = i.replace("\n","").replace("\t","").replace("\r","")
+        p = re.compile("(?<=\<table class\=\"tbl_data tbl_data_col\">)(.*?)(?=<\/table>)",re.DOTALL)
+        m = p.findall(response)
+        trTags = {}
+        key = {}
+        for i in range(len(m)):
+            m[i] = m[i].replace("\n","").replace("\t","").replace("\r","")
+            #parse thead tag
+            p = re.compile("(?<=\<thead>)(.*?)(?=<\/thead>)")
+            thead = p.findall(m[i])[0]
+            p = re.compile("(?<=\<th scope\=\"col\">)(.*?)(?=<\/th>)")
+            key[str(i+1)+"st"] = p.findall(thead)
+            #parse tbody tag
+            p = re.compile("(?<=\<tbody>)(.*?)(?=<\/tbody>)")
+            tbody = p.findall(m[i])[0]
             #parse tr tag
             p = re.compile("(?<=\<tr>)(.*?)(?=<\/tr>)")
-            a = p.findall(i)
-            arr.append(a)
-        key = {}
-        td = {}
-        #parse th tag
-        for i in range(len(arr)):
-            td[str(i+1) + "st"] = {}
-            for j in range(len(arr[i])):
-                p = re.compile("(?<=\<th scope\=\"col\">)(.*?)(?=<\/th>)")
-                if p.search(arr[i][j]):
-                    key[str(i+1) + "st"] = p.findall(arr[i][j])
-                else:
-                    #parse td tag
-                    p = re.compile("(?<=\<td>)(.*?)(?=<\/td>)")
-                    if p.search(arr[i][j]):
-                        td[str(i+1) + "st"][j] = p.findall(arr[i][j])
-                    
-                    p = re.compile("(?<=\<td class=\"lt\">)(.*?)(?=<\/td>)", re.DOTALL)
-                    if p.search(arr[i][j]):
-                        td[str(i+1) + "st"][j] = p.findall(arr[i][j])
-        
-        #save data
-        for k1,v1 in td.items():
-            for k, v in v1.items():
-                p = re.compile("(?<=\<a href=\"\#\" class=\"btn_search\" onClick=\"javascript\:showMapPage\(\\')(.*?)(?=\\'\)\")")
-                for i in range(len(v)):
-                    if p.search(v[i]):
-                        td[k1][k][i] = p.findall(v[i])[0]
-        print(key)
-        print(td)
-        st1 = td["1st"]
-        print(len(st1))
-    
+            trTags[str(i+1)+"st"] = p.findall(tbody)
+        tdTags = {}
+        for k, v in trTags.items():
+            #(\<tr>)(.*?)(<\/tr>) 전방탐색, 후방탐색을 사용하면 <td></td> 같은 경우 가져오지 못하고 뒤에것과 겹친다
+            p = re.compile("(\<td>)(.*?)(<\/td>)")
+            classLt = re.compile("(\<td\sclass=\"lt\">)(.*?)(\<\/td>)")
+            latiLongi = re.compile("(\<a href=\"\#\" class=\"btn_search\" onClick=\"javascript\:showMapPage\(\\')(.*?)(\\'\)\")")
+            tdTags[k] = []
+            tdTag = {}
+            for tr in range(len(v)):
+                td = p.findall(v[tr])
+                for i in range(len(td)):
+                    if "javascript" in td[i][1]:
+                        td[i] = latiLongi.findall(td[i][1])[0]
+                    tdTag[key[k][i]] = td[i][1]
+                td = classLt.findall(v[tr])
+                for i in range(len(td)):
+                    tdTag[key[k][2]] = td[i][1]
+                tdTags[k].append(tdTag.copy())
+                print(tdTags)
+                print("========")
+            
+        print(tdTags)
     def test_long_lat(self):
         valueHistory = {1: ['1', '스파', '자동 ', '서울 노원구 동일로 1493 상계주공아파트(10단지) 주공10단지종합상가111', '11100773']}
         winInfo = lotto_win_history_crawling.WinInfoUtil()
